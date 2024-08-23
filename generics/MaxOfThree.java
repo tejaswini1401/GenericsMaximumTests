92360cd875525ca873517b2d973e9edbb7e08c2d
tejaswini_14 <shelaketejaswini14@gmail.com>
2024-08-23 17:50:15 +0530

    Added: find maxium of Integer, float and String using Generics

--- generics/MaxOfThree.java
+++ generics/MaxOfThree.java
@@ -1,8 +1,9 @@
 package com.generics;
+
 public class MaxOfThree {
 
-	public static String findMaximum(String a, String b, String c) {
-        String max = a;
+    public static <T extends Comparable<T>> T findMaximum(T a, T b, T c) {
+        T max = a;
 
         if (b.compareTo(max) > 0) {
             max = b;
@@ -15,11 +16,21 @@ public class MaxOfThree {
     }
 
     public static void main(String[] args) {
-        String a = "apple";
-        String b = "banana";
-        String c = "Leamon";
+        Integer int1 = 10;
+        Integer int2 = 20;
+        Integer int3 = 30;
+        System.out.println("Max Integer: " + findMaximum(int1, int2, int3));
 
-        String result = findMaximum(a, b, c);
-        System.out.println("The maximum string is: " + result);
+        String str1 = "apple";
+        String str2 = "banana";
+        String str3 = "cherry";
+        System.out.println("Max String: " + findMaximum(str1, str2, str3));
+        
+        float a = 10.5f;
+        float b = 20.6f;
+        float c = 30.57f;
+
+        float result = findMaximum(a, b, c);
+        System.out.println("Maximum Float: " + result);
     }
 }
